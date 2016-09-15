6a47d8f343bc6ba06d94ca022b82b428bf5b17fa
Chris Chiang <cchiang1992@gmail.com>
2016-09-15 14:58:25 -0700

    Option to add comments to friends posts
    scene not rendered yet

--- client/journalapp/app/Entry_Components/Entry.js
+++ client/journalapp/app/Entry_Components/Entry.js
@@ -27,7 +27,7 @@ var parseDate = (date) => {
 var moddedStyle = {
   borderBottomWidth: 0.5,
   borderColor: '#cccccc',
-  paddingTop: 34,
+  paddingTop: 40,
   paddingBottom:12
 }
 
@@ -89,6 +89,20 @@ export default class Entry extends Component {
     };
   }
 
+  allowComments() {
+    if (this.props.friendPost) {
+      return (
+        <TouchableHighlight style={ styles.commentContainer } underlayColor='#dcdcdc' onPress={ this.addComment }>
+          <Text style={ styles.rating } style={{color: 'blue'}} >Comment</Text>
+        </TouchableHighlight>
+      )
+    }
+  }
+
+  addComment() {
+    console.log('yo dawg');
+  }
+
   render() {
     return (
       <View style={ this.state.entryStyle }>
@@ -105,14 +119,19 @@ export default class Entry extends Component {
             <Text style={ styles.entryText }>
               { this.props.text }     
             </Text>
-            <TouchableHighlight style={ styles.ratingContainer } onPress={ this.likePost }>
-              <View style={ styles.ratingContainer }>
-                <Text style={ styles.rating }>
-                  { this.props.votes.length }
-                </Text>
-                <Image style={ styles.image } source={ require('../images/empty_heart.png') }></Image>
-              </View>
-            </TouchableHighlight>
+            <View style={ styles.commentratingContainer }>
+            
+              { this.allowComments() }
+
+              <TouchableHighlight style={ styles.ratingContainer } onPress={ this.likePost }>
+                <View style={ styles.ratingContainer }>
+                  <Text style={ styles.rating }>
+                    { this.props.votes.length }
+                  </Text>
+                  <Image style={ styles.image } source={ require('../images/empty_heart.png') }></Image>
+                </View>
+              </TouchableHighlight>
+            </View>
           </View>
         </View>
       </View>
